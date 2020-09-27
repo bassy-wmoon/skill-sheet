5bd80f969786b450b8e3c7e3e39e41806fcb9691
tomoharu-ishibashi <double.moon84@gmail.com>
2020-09-27 20:28:39 +0900

    comment out layout style

--- layouts/preview.vue
+++ layouts/preview.vue
@@ -5,19 +5,19 @@
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 0.75rem;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+/*html {*/
+/*  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',*/
+/*    Roboto, 'Helvetica Neue', Arial, sans-serif;*/
+/*  font-size: 0.75rem;*/
+/*  word-spacing: 1px;*/
+/*  -ms-text-size-adjust: 100%;*/
+/*  -webkit-text-size-adjust: 100%;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  box-sizing: border-box;*/
+/*}*/
 
-body {
-  font-size: 1rem;
-}
+/*body {*/
+/*  font-size: 1rem;*/
+/*}*/
 </style>
